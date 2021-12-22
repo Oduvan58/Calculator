49dd0d9e9f7f5b6e3fae6ba6b442821c9f3a90c0
Oduvan58 <yancheuskipavel@gmail.com>
2021-12-22 18:42:03 +0300

    Add saving input

--- app/src/main/java/by/geekbrains/calculator/MainActivity.java
+++ app/src/main/java/by/geekbrains/calculator/MainActivity.java
@@ -1,27 +1,36 @@
 package by.geekbrains.calculator;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String NUMBER_SAVE_KEY = "number_key";
 
     private TextView workingTextView;
     private TextView resultTextView;
 
-    private Button zeroButton, oneButton, twoButton, threeButton, fourButton, fiveButton
-            , sixButton, sevenButton, eightButton, nineButton, clearButton;
+    private Button zeroButton, oneButton, twoButton, threeButton, fourButton, fiveButton, sixButton
+            , sevenButton, eightButton, nineButton, clearButton;
+
+    private long number;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         initViews();
-        symbolOnClick();
+
+        if (savedInstanceState != null && savedInstanceState.containsKey(NUMBER_SAVE_KEY)) {
+            number = savedInstanceState.getLong(NUMBER_SAVE_KEY);
+        }
+
+        showInput();
     }
 
     private void initViews() {
@@ -38,6 +47,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         eightButton = findViewById(R.id.number_eight_button);
         nineButton = findViewById(R.id.number_nine_button);
         clearButton = findViewById(R.id.clear_button);
+        symbolOnClick();
     }
 
     private void symbolOnClick() {
@@ -54,7 +64,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         clearButton.setOnClickListener(this);
     }
 
-    @SuppressLint("NonConstantResourceId")
     @Override
     public void onClick(View view) {
         String working = workingTextView.getText().toString();
@@ -70,11 +79,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.number_eight_button:
             case R.id.number_nine_button:
                 working = working + ((Button) view).getText();
-                workingTextView.setText(working);
+                number = Long.parseLong(working);
+                showInput();
                 break;
             case R.id.clear_button:
                 workingTextView.setText("");
                 break;
         }
+    }
+
+    private void showInput() {
+        workingTextView.setText(String.valueOf(number));
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putLong(NUMBER_SAVE_KEY, number);
     }
 }
